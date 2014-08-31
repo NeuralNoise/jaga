fccb1b71574caff93e13e49e1446526362e18550
Christopher Webb <support@seattledata.net>
2014-08-31 16:09:11 -0700

    file name revisions

--- jaga/php/config/config.php
+++ jaga/php/config/config.php
@@ -8,42 +8,42 @@
 	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/controller/Session.php');
 
 	// MODEL
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Audit.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Authentication.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Calendar.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Category.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Channel.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/ChannelCategory.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Comment.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Content.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Cookie.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/File.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Form.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Image.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Language.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Mail.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Message.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/RSS.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/SEO.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Shop.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Sitemap.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Subscription.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/Theme.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/User.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/audit.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/authentication.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/calendar.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/category.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/channel.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/channelCategory.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/comment.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/content.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/cookie.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/file.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/form.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/image.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/language.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/mail.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/message.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/rss.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/seo.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/shop.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/sitemap.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/subscription.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/theme.class.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/model/user.class.php');
 	
 	// VIEW
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/AuthenticationView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/CarouselView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/CategoryView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/ChannelView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/CommentView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/ContentView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/MenuView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/PageView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/ProfileView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/SubscriptionView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/ThemeView.php');
-	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/UserView.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/authentication.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/carousel.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/category.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/channel.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/comment.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/content.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/menu.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/page.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/profile.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/subscription.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/theme.view.php');
+	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/view/user.view.php');
 
 	// DB: apply gitignore to taterbase.php
 	require($_SERVER['DOCUMENT_ROOT'] . '/jaga/php/config/taterbase.php');
