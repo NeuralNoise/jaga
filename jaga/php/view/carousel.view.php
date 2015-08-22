dd6fc6e611233ee976f16b4103c5dc1e1192e0e8
Chishiki <chishiki@gmail.com>
2015-08-22 15:15:26 -0700

    carousel revisions

--- jaga/php/view/carousel.view.php
+++ jaga/php/view/carousel.view.php
@@ -5,46 +5,67 @@ class CarouselView {
 	public function getCarousel() {
 	
 		$html = "\t\t<div class=\"container\" style=\"margin-bottom:20px;\">
-			<div id=\"kutchannel-carousel\" class=\"carousel slide\" data-ride=\"carousel\">
+			<div id=\"kutchannel-carousel\" class=\"carousel slide\" data-ride=\"carousel\">\n";
 
+				/*
+				$html .= "
 				<!-- Indicators -->
+				<!--
 				<ol class=\"carousel-indicators\">
 					<li data-target=\"#kutchannel-carousel\" data-slide-to=\"0\" class=\"active\"></li>
 					<li data-target=\"#kutchannel-carousel\" data-slide-to=\"1\"></li>
 					<li data-target=\"#kutchannel-carousel\" data-slide-to=\"2\"></li>
+					<li data-target=\"#kutchannel-carousel\" data-slide-to=\"3\"></li>
 				</ol>
-
+				-->
+				";
+				*/
+				
+			$html .= "
+			
 				<!-- START SLIDES -->\n\n";
 				
-				if ($_SESSION['channelID'] == '2006') { // jaga.io
+				if ($_SESSION['channelKey'] == 'www') { // jaga.io
 					$html .= "\t\t\t\t<div class=\"carousel-inner\">
 						<div class=\"item active\"><a href=\"http://hakodate.jaga.io/\"><img src=\"/jaga/images/carousel-hakodate-goryokaku.jpg\" alt=\"test0\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
 						<div class=\"item\"><a href=\"http://kagi.io/\"><img src=\"/jaga/images/carousel-zenidev.jpg\" alt=\"test1\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
 						<div class=\"item\"><a href=\"http://kids.jaga.io/\"><img src=\"/jaga/images/carousel-ballpit.jpg\" alt=\"test2\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
 					</div>\n\n";
-				} elseif ($_SESSION['channelID'] == '14') { // The Kutchannel
+				} elseif ($_SESSION['channelKey'] == 'niseko') { // The Kutchannel
 					$html .= "\t\t\t\t<div class=\"carousel-inner\">
 						<div class=\"item active\"><img src=\"/jaga/images/carousel-mountain.jpg\" alt=\"test0\" style=\"margin-left:auto;margin-right:auto;\"></div>
 						<div class=\"item\"><a href=\"http://kagi.io/\"><img src=\"/jaga/images/carousel-zenidev.jpg\" alt=\"test1\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
-						<div class=\"item\"><a href=\"http://www.lecochon-niseko.com/\"><img src=\"/jaga/images/carousel-roll-tide.jpg\" alt=\"test2\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
+						<div class=\"item\"><a href=\"http://www.lecochon-niseko.com/\" target=\"_blank\"><img src=\"/jaga/images/carousel-roll-tide.jpg\" alt=\"test2\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
+						<div class=\"item\"><a href=\"http://holidayniseko.com/\" target=\"_blank\"><img src=\"/jaga/images/carousel-holidayniseko.jpg\" alt=\"test3\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
 					</div>\n\n";
-				} elseif ($_SESSION['channelID'] == '21') { // Hakodate Guide
+				} elseif ($_SESSION['channelKey'] == 'hakodate') { // Hakodate Guide
 					$html .= "\t\t\t\t<div class=\"carousel-inner\">
 						<div class=\"item active\"><img src=\"/jaga/images/carousel-hakodate-goryokaku.jpg\" alt=\"test0\" style=\"margin-left:auto;margin-right:auto;\"></div>
 						<div class=\"item\"><img src=\"/jaga/images/carousel-hakodate-ishikawa.jpg\" alt=\"test1\" style=\"margin-left:auto;margin-right:auto;\"></div>
-						<div class=\"item\"><a href=\"http://kagi.io/\"><img src=\"/jaga/images/carousel-zenidev.jpg\" alt=\"test3\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
+						<div class=\"item\"><a href=\"http://kagi.io/\"><img src=\"/jaga/images/carousel-zenidev.jpg\" alt=\"test2\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
+					</div>\n\n";
+				} elseif ($_SESSION['channelKey'] == 'shoreline' || $_SESSION['channelKey'] == 'seattle') { // Local
+					$html .= "\t\t\t\t<div class=\"carousel-inner\">
+						<div class=\"item active\"><a href=\"http://refer.vonage.com/v2/share/6185926949894533561\" target=\"_blank\"><img src=\"/jaga/images/carousel-vonage.jpg\" alt=\"test0\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
+						<div class=\"item\"><a href=\"https://nextdoor.com/invite/ysarxeamsfbznkkmzhut\" target=\"_blank\"><img src=\"/jaga/images/carousel-nextdoor.jpg\" alt=\"test1\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
+						<div class=\"item\"><a href=\"http://kagi.io/\"><img src=\"/jaga/images/carousel-zenidev.jpg\" alt=\"test2\" style=\"margin-left:auto;margin-right:auto;\"></a></div>
 					</div>\n\n";
 				}
 
-				$html .= "\t\t\t<!-- END SLIDES -->
+				$html .= "\t\t\t<!-- END SLIDES -->\n\n";
 				
-				<!-- Controls -->
-				<a class=\"left carousel-control\" href=\"#kutchannel-carousel\" data-slide=\"prev\"><span class=\"glyphicon glyphicon-chevron-left\"></span></a>
-				<a class=\"right carousel-control\" href=\"#kutchannel-carousel\" data-slide=\"next\"><span class=\"glyphicon glyphicon-chevron-right\"></span></a>
-
-			</div>
-		</div>
-		";
+				/*
+				$html .= "
+					<!-- Controls -->
+					<!--
+					<a class=\"left carousel-control\" href=\"#kutchannel-carousel\" data-slide=\"prev\"><span class=\"glyphicon glyphicon-chevron-left\"></span></a>
+					<a class=\"right carousel-control\" href=\"#kutchannel-carousel\" data-slide=\"next\"><span class=\"glyphicon glyphicon-chevron-right\"></span></a>
+					-->
+				";
+				*/
+				
+				$html .= "\t\t\t</div>\n\t\t</div>";
+				
 		return $html;
 	}
 
