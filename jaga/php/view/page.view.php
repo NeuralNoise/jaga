8ee0d4b5191508bad91680f5deeb28434b90cba2
Chishiki <chishiki@gmail.com>
2017-06-28 00:01:13 -0700

    added google maps API key

--- jaga/php/view/page.view.php
+++ jaga/php/view/page.view.php
@@ -155,7 +155,10 @@ class PageView {
 				$html .= "\t\t<link rel=\"stylesheet\" type=\"text/css\" href=\"/channel.css\" />\n\n";
 
 				$html .= "\t\t<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>\n";
-				$html .= "\t\t<script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false&libraries=places\"></script>\n";
+				
+				$html .= "\t\t<script type=\"text/javascript\" async defer src=\"https://maps.googleapis.com/maps/api/js?key=" . Config::read('googlemaps.embed-api-key') . "\"></script>\n";
+				// $html .= "\t\t<script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false&libraries=places\"></script>\n";
+				
 				$html .= "\t\t<script type=\"text/javascript\" src=\"/jaga/lib/bootstrap/3.3.2/js/bootstrap.min.js\"></script>\n";
 				$html .= "\t\t<script type=\"text/javascript\" src=\"/jaga/lib/masonry/masonry.pkgd.min.js\"></script>\n";
 				$html .= "\t\t<script type=\"text/javascript\" src=\"/jaga/lib/locationpicker/locationpicker.jquery.js\"></script>\n";
