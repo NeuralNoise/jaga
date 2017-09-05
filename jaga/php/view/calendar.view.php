cea65064fd1dd5776dc7c936e0750865e18fe0b0
Chishiki <chishiki@gmail.com>
2017-09-04 22:46:12 -0700

    added some basic navigation to the month calendar

--- jaga/php/view/calendar.view.php
+++ jaga/php/view/calendar.view.php
@@ -5,24 +5,64 @@ class CalendarView {
 	public function displayCalendar($yearMonth,$channelID) {
 
 		// selected month
-		$d = new DateTime($yearMonth);
-		$cal_header = $d->format('F Y');
-		$first_weekday_of_month = $d->format('N');
-		$last_day_of_month = $d->format('t');
+		$selected_month = new DateTime($yearMonth);
+		$cal_header = $selected_month->format('F Y');
+		$first_weekday_of_month = $selected_month->format('N');
+		$last_day_of_month = $selected_month->format('t');
+
+		// previous year
+		$prev_year = new DateTime($yearMonth);
+		$prev_year->modify('-1 year');
+		$previousYear = $prev_year->format('Y-m');
+
+		// previous month
+		$prev_month = new DateTime($yearMonth);
+		$prev_month->modify('first day of previous month');
+		$last_day_of_previous_month = $prev_month->format('t');
+		$previousMonth = $prev_month->format('Y-m');
+		
+		// next month
+		$next_month = new DateTime($yearMonth);
+		$next_month->modify('first day of next month');
+		$nextMonth = $next_month->format('Y-m');
+		
+		// next year
+		$next_year = new DateTime($yearMonth);
+		$next_year->modify('+1 year');
+		$nextYear = $next_year->format('Y-m');
+		
+		// counter utilities
 		$date_counter = 1;
 		$days_left = $last_day_of_month;
 		
-		// previous month
-		$d->modify('first day of previous month');
-		$last_day_of_previous_month = $d->format('t');
 		$end_of_previous_month_counter = $last_day_of_previous_month - $first_weekday_of_month;
 		
 		$h = '<div class="container">';
 			$h .= '<div class="row">';
 				$h .= '<div class="col-xs-12 calMonth">';
-					
-					$h .= '<div class="calMonthHeader">' . $cal_header . '</div>';
-					
+
+					$h .= "<div class=\"row\">";
+						$h .= "<div class=\"col-xs-4\">";
+							$h .= "<a class=\"btn btn-default accountNavButton\" href=\"/calendar/" . $previousYear . "/\">";
+								$h .= "<span class=\"glyphicon glyphicon-fast-backward\"></span>";
+							$h .= "</a>";					
+							$h .= "<a class=\"btn btn-default accountNavButton\" href=\"/calendar/" . $previousMonth . "/\">";
+								$h .= "<span class=\"glyphicon glyphicon-step-backward\"></span>";
+							$h .= "</a>";
+						$h .= "</div>";
+						
+						$h .= "<div class=\"col-xs-4 text-center\">" . $cal_header . "</div>";
+						
+						$h .= "<div class=\"col-xs-4 text-right\">";
+							$h .= "<a class=\"btn btn-default pull-right accountNavButton\" href=\"/calendar/" . $nextYear . "/\">";
+								$h .= "<span class=\"glyphicon glyphicon-fast-forward\"></span>";
+							$h .= "</a>";
+							$h .= "<a class=\"btn btn-default pull-right accountNavButton\" href=\"/calendar/" . $nextMonth . "/\">";
+								$h .= "<span class=\"glyphicon glyphicon-step-forward\"></span>";
+							$h .= "</a>";
+						$h .= "</div>";
+					$h .= "</div>";
+
 					$h .= '<div class="calMonthWeek calMonthDaysOfWeek">';
 						for ($x = -1; $x < 6; $x++) {
 							$h .= '<div class="calMonthDay calMonthDayOfWeek">' . jddayofweek($x,2) . '</div>';
@@ -79,7 +119,6 @@ class CalendarView {
 		return $h;
 
 	}
-
 
 }
 
